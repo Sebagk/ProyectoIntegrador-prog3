0893833a62b2f46d4cfeb0b213763940a2d7988c
Sebagk <garciakatzs@udesa.edu.ar>
2024-09-16 16:30:55 -0300

    home

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'
+import Home from "./pages/Home";
 import Footer from "./components/Footer/Footer";
 import Error404 from "./pages/Error404";
 function App() {
   return (
     <div className="App">
-      <p>Hola react</p>
       <Switch>
+        <Route path = "/" exact component = {Home}/>
         <Route component={Error404} />
       </Switch>
       <Footer />
