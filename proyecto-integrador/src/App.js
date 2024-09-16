3997c48d27dd164487a1d138f8d1b00cf2d7b38e
andresfernandez1 <andresfernandez@udesa.edu.ar>
2024-09-16 15:32:59 -0300

    components y footer

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <p>Hola react</p>
+      <Footer />
     </div>
   );
 }
---
+++ proyecto-integrador/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css"
+
+function Footer(){
+    return(
+        <footer>
+        <p className="footer">Andrés Fernández-Sebastian Garcia Katz-Lisandro Zabala</p>
+    </footer>
+    )
+}
+
+export default Footer;
