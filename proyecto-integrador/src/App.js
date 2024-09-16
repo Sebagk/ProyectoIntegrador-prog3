6ea57f79aac9816ca61b62d2082a48da3dce1554
Sebagk <garciakatzs@udesa.edu.ar>
2024-09-16 15:52:02 -0300

    pages

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
-
+import Error404 from "./pages/Error404";
 function App() {
   return (
     <div className="App">
       <p>Hola react</p>
+      <Switch>
+        <Route component={Error404} />
+      </Switch>
       <Footer />
     </div>
   );
