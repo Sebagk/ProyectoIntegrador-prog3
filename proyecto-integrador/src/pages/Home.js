5d33f935e975e28fef11c0fef18018e3f56931ff
andresfernandez1 <andresfernandez@udesa.edu.ar>
2024-09-23 20:53:49 -0300

    muy avanzado el home con cambios nuevos

--- proyecto-integrador/src/pages/Home.js
+++ proyecto-integrador/src/pages/Home.js
@@ -1,33 +1,38 @@
 import React, { Component } from 'react'
 import SearchForm from '../components/SearchForm/SearchForm'
 import CardGrid from "../components/CardGrid/CardGrid"
+import { options } from '../options'
 
 class home extends Component {
   constructor(props){
     super(props)
     this.state = {
-      populares: null,
-      estrenos: null
+      populares: [],
+      estrenos: []
     }
   }
   componentDidMount(){
     fetch("https://api.themoviedb.org/3/movie/popular", options)
     .then(response => response.json())
-    .then(data => this.setState({
+    .then(data => {
+      console.log(data);
+      
+      this.setState({
       populares: data.results,
-      isLoading: false
-    }))
+    })})
     .catch((error) => {
       console.log(error);
     })
   
   fetch("https://api.themoviedb.org/3/movie/now_playing", options)
     .then(response => response.json())
-    .then(data => this.setState({
+    .then(data => {
+      console.log(data);
+      
+      this.setState({
       estrenos: data.results,
-      filteredMovies: data.results,
-      isLoading: false
-    }))
+
+    })})
     .catch((error) => {
       console.log(error);
     })
@@ -38,7 +43,10 @@ class home extends Component {
   render() {
     return (
       <>
-      <CardGrid  link={this.state.populares}/>
+      <h1>Populares</h1>
+      <CardGrid link={"/populares"} cantidad={5} peliculas={this.state.populares}/>
+      <h1>Estrenos</h1>
+      <CardGrid link={"/estrenos"} cantidad={5} peliculas={this.state.estrenos}/>
       </>
     )
   }}
