981f0725db0e7337301b22fc6be3787e38d7c47b
Sebagk <garciakatzs@udesa.edu.ar>
2024-09-23 16:07:39 -0300

    searchResults

--- proyecto-integrador/src/pages/SearchResults.js
+++ proyecto-integrador/src/pages/SearchResults.js
@@ -1,14 +1,39 @@
 import {Component} from 'react'
+import { Link } from 'react-router-dom'
+import { options } from '../options'
+import Card from '../components/Card/Card'
 
 export default class SearchResults extends Component{
 
     constructor(props){
         super(props)
+        this.state ={
+          resultados: []
+        }
+    }
+
+    componentDidMount(){
+      const { query } = this.props.location.state;
+      if(query){
+        fetch(`https://api.themoviedb.org/3/search/movie?query=${query}`, options)
+        .then(response => response.json())
+        .then(data => this.setState({ resultados: data.results }))
+        .catch((e) => console.log(e))
+        
+
+      }
     }
 render(){
   return (
-    // <div> searchResults {this.location.state.query}</div>
-    <p>hola</p>
+    <div>
+      {this.state.resultados.length > 0 ? (
+          this.state.resultados.slice(0, 20).map((resultado, index) => (
+            <Card pelicula = {resultado} key={index}/>
+          ))
+        ) : (
+          <h1>No hay resultados</h1> 
+        )}
+    </div>
   )
     }
 } 
